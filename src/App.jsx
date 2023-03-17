a7d84234aae3e30d406d65f97071d1780533c34f
rei_kaji <st.r.z.rei@gmail.com>
2023-03-17 12:48:35 -0700

    add access counter

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,19 @@ function App() {
           <Contact />
           <StarsCanvas />
         </div>
+        <footer className="flex flex-row gap-2 justify-center items-center">
+          {/* Add Access Counter 
+          URL: https://script.google.com/u/0/home/projects/1sIppfiecXOI_DNpSsGNDrRmoLMss7qlaoq3S5FCNtCdDu9aGtfpgr9FL/edit
+          */}
+          <h6>Access Count: </h6>
+          <iframe
+            loading="lazy"
+            src="https://script.google.com/macros/s/AKfycbwlMsH5yJbxIppDJXooGLf5ihPAOCCAawa6OuxYgDoeV7zy6gI_SmwJWl5eWPR-V1M0/exec?url=https://rei-portfolio.netlify.app/"
+            title="AccessCounter"
+            scrolling="no"
+            frameBorder="0"
+          ></iframe>
+        </footer>
       </div>
     </BrowserRouter>
   );
